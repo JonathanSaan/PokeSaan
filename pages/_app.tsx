8d941319f11372731e6e1261fff4b7abe72b6a28
JonathanSaan <crosspool03@gmail.com>
2022-10-14 12:24:32 -0300

    up

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from "next/app";
+import GlobalStyle from "../styles/global";
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
---
+++ styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    padding: 0;
+    margin: 0;
+  }
+`;
