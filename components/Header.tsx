1dec8196c116092a1215f1276d6c601468b13cc8
JonathanSaan <crosspool03@gmail.com>
2022-10-15 10:56:34 -0300

    up

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Link from "next/link";
+import { BiSearch } from "react-icons/bi";
+import { Container, Search, Title } from "../styles/header";
+
+export const Header = () => {
+  
+  return (
+    <Container>
+      <Link href="/">
+        <Title>PokeSaan</Title>
+      </Link>
+      <Search>
+        <input placeholder='Search...' type="text"/>
+        <i>
+          <BiSearch size={25} />
+        </i>
+      </Search>
+    </Container>
+  );
+};
---
+++ styles/header.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  padding: 0 1em;
+  height: 17vw;
+  width: 100%;
+  background-color: #1D1F2B;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Search = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: flex-end;
+  align-items: center;
+  
+  input[type=text] {
+    position: relative;
+    padding: .6em 1.3em .6em 1.3em;
+    margin: 0 0 0 auto;
+    width: 2em;
+    color: #525252;
+    text-transform: uppercase;
+    font-size: 16px;
+    font-weight: 100;
+    letter-spacing: 2px;
+    border: none;
+    border-radius: 5px;
+    background: linear-gradient(to right, #FFFFFF 0%,#464747 #F9F9F9 100%);
+    transition: width 0.4s ease;
+    outline: none;
+    
+    &:focus { 
+      width: 80%;
+    }
+  }
+  
+  i {
+    position: relative;
+    margin: 0 .5em -.1em -2em;
+    color: #1D1F2B;
+  }
+}
+`;
+
+export const Title = styled.a`
+  text-decoration: none;
+  color: #FFF;
+  font-size: 5vw;
+  font-weight: 700;
+`;
