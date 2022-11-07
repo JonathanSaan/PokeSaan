533c0293b8d58bf5dc35d3e633fd6ab97716d5da
JonathanSaan <crosspool03@gmail.com>
2022-11-07 18:44:23 -0300

    up

--- styles/home.ts
+++ styles/home.ts
@@ -12,7 +12,7 @@ export const Container = styled.div`
 export const AllPokemons = styled.div`
   display: flex;
   flex-wrap: wrap;
-  padding: 1vw 0;
+  margin: .5em 0;
 `;
 
 export const OnePokemon = styled.div`
